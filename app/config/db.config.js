a89b6a2c96d6585a7b75b9e0970699cf9cd3f9b1
almas615 <almasadlilwafi615@gmail.com>
2021-10-30 11:34:21 +0700

    edit db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// setup database here, change the values to suit your environment
-module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "postgres",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
-=======
 // setup database here, change the values to suit your environment
 module.exports = {
   HOST: "localhost",
@@ -28,4 +12,3 @@ module.exports = {
     idle: 10000
   }
 };
->>>>>>> baf648225364f9445b88ec44af4bd35447881e3d
