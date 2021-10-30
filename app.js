ff83d2ebfef68d1857f0ddeceb818b7d4815eaf7
almas615 <almasadlilwafi615@gmail.com>
2021-10-30 11:35:14 +0700

    edit app js

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
-const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// accept request in form or JSON
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const db = require("./app/models");
-db.client.sync();
-
-require("./app/routes/player.routes")(app);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-=======
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -59,4 +33,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
->>>>>>> baf648225364f9445b88ec44af4bd35447881e3d
+
